dccdc7b713d4527aec8b0e89dc7b84894611bfcc
whdgns0332 <whdgns0332@naver.com>
2020-06-15 21:08:33 +0900

    Generic Souce 수정

---
+++ ClockApp/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ClockApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            DateTime now = DateTime.Now;
+            label1.Text = now.ToString("HH:mm:ss");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("로드");
+        }
+
+        private void Form1_FormClosing_1(object sender, FormClosingEventArgs e)
+        {
+            if (MessageBox.Show("진짜 닫을래?", "경고",
+                    MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+            {
+                e.Cancel = false; // 취소를 안시켜서 종료
+            }
+            else
+            {
+                e.Cancel = true; // 취소를 시켜서 종료안됨
+            }
+
+        }
+        /*
+        private void Form1_Activated(object sender, EventArgs e)
+        {
+            MessageBox.Show("활성화");
+        }
+        */
+    }
+}
---
+++ WinFormApp/MainForm.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormApp
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DateTime now = DateTime.Now;
+            //MessageBox.Show($"Hello World! {now}");
+            TxtCurrentDate.Text = now.ToString();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
