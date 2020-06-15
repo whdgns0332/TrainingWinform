43666c0d0db180190bd739a66255561be084ac3f
whdgns0332 <whdgns0332@naver.com>
2020-06-15 12:30:51 +0900

    Generic Source

--- GenericApp/Program.cs
+++ GenericApp/Program.cs
@@ -3,13 +3,52 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Markup;
 
 namespace GenericApp
 {
+    public class SimpleGeneric<T>
+    {
+        private T[] valuse;
+        private int index;
+
+        public SimpleGeneric(int len)
+        {
+            valuse = new T[len];
+            index = 0;
+        }
+
+        public void Add(params T[] args)
+        {
+            foreach (T item in args)
+            {
+                valuse[index++] = item;
+            }
+        }
+
+        public void Print()
+        {
+            foreach (T item in valuse)
+            {
+                Console.Write(item + ", ");
+            }
+            Console.WriteLine();
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
+            SimpleGeneric<Int32> gIntergers = new SimpleGeneric<int>(10);
+            SimpleGeneric<double> gDoubles = new SimpleGeneric<double>(10);
+
+            gIntergers.Add(1, 2);
+            gIntergers.Add(1, 2, 3, 4, 5, 6, 7);
+            gIntergers.Add(10);
+
+            gDoubles.Add(10.0, 12.4, 37.5);
+            gIntergers.Print();
+            gDoubles.Print();
         }
     }
 }
